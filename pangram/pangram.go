56611252e493cced6bc96918545d991a867e54a8
Victor Wibisono <victor@eatigo.com>
2017-07-13 11:24:12 +0800

    Pangram solved.

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,7 +1,18 @@
 package pangram
 
+import "strings"
+
 const testVersion = 1
 
 func IsPangram(i string) bool {
-	return true;
+	alphabets := "abcdefghijklmnopqrstuvwxyz"
+	alphabet_array := strings.Split(alphabets, "")
+
+	for _, v := range alphabet_array {
+		if strings.ContainsAny(strings.ToLower(i), v) == false {
+			return false
+		}
+	}
+
+	return true
 }
